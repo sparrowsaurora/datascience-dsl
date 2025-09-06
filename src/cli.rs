f005d8723561dfb323ad0e2b02322985804e4a72
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2025-09-06 21:40:21 +0800

    (feat): made 3 basic file commands

--- src/cli.rs
+++ src/cli.rs
@@ -1,1 +1,18 @@
 // Command-line parsing using clap
+
+const command_name: &str = "dpl";
+
+#[derive(Parser)]
+#[command(name = command_name)]
+pub struct Cli {
+    pub file: Option<String>,
+
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Repl,
+    Explain { file: String },
+}
